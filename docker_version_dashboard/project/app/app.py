73254a1848bf38d8bcf9a4b0aedb5447eca8db39
CBlumini <72533588+CBlumini@users.noreply.github.com>
2024-09-07 14:55:15 -0400

    Major code cleanup

--- docker_version_dashboard/project/app/app.py
+++ docker_version_dashboard/project/app/app.py
@@ -1,24 +1,16 @@
-# this might help me break the project up
-# https://www.purfe.com/dash-project-structure-multi-tab-app-with-callbacks-in-different-files/
-
-# https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/
-
 import pandas as pd
 import dash
 from dash import dcc
 from dash import html
 import plotly.express as px
-import pandas as pd
-import plotly.graph_objects as go
-from dash import dash_table
+import dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from convert_time import *
+import plotly.graph_objects as go
 
-# stop pandas from issuing ceratain warnings
+# stop pandas from issuing certain warnings
 pd.options.mode.chained_assignment = None  # default='warn'
-
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
@@ -33,15 +25,12 @@ colors = {
 data = pd.read_csv(
     "https://raw.githubusercontent.com/CBlumini/Triathlon_Dashboarding_and_Scraping/main/data/Santa-Cruz-Sprint.csv", header=0, index_col=None)
 data = data[data['Age'] > 1]
-# females = data[data['Gender'] == 'F']
 females = data
 
 datapie = data
 datapie['Age Group'] = datapie.apply(determine_agegroup, axis=1)
 
-# the data does not come in the right form to do math on it. So convert the times to minutes and decimal seconds
-# maybe setup a compute file to do this by itself later
-
+# Convert times for math calculations
 time_df = create_time_columns(females)
 
 reduced2 = time_df[["Name", "Swim Minutes", "Swim+T1",
@@ -51,8 +40,7 @@ reduced2 = reduced2[reduced2['Total'] > 60]
 
 # name the columns for the data table
 dash_columns = ["Bib", "Name", "Age", "Gender", "City", "Swim", "T1", "Bike", "T2", "Run", "Chip Elapsed", "Div Place",
-                "Age Place",
-                "Gender Place"]
+                "Age Place", "Gender Place"]
 
 # layout the page
 app.layout = html.Div([
@@ -64,10 +52,6 @@ app.layout = html.Div([
             dbc.Col(
                 html.H6(children='This app allows for performance plotting of certain local bay area triathlons.'))
         ]),
-        # dbc.Row([
-        #     dbc.Col(html.H6(children='This is a work in progress'
-        #                              ' Now with case-INSENSITIVE searching'))
-        # ]),
         dbc.Row([
             dash_table.DataTable(
                 id='table-sorting-filtering',
@@ -80,11 +64,11 @@ app.layout = html.Div([
                 },
                 style_cell={
                     'height': '90',
-                    # 'minWidth': '110%',
                     'minWidth': '60px', 'width': '100px', 'maxWidth': '140px',
                     'whiteSpace': 'normal', 'textAlign': 'center',
                     'backgroundColor': 'rgb(0, 0, 0)',
-                    'color': 'white'},
+                    'color': 'white'
+                },
                 style_cell_conditional=[{
                     'if': {'column_id': 'Name'},
                     'textAlign': 'center'
@@ -93,27 +77,22 @@ app.layout = html.Div([
                 page_size=15,
                 filter_action='native',
                 filter_options={'case': 'insensitive'},
-                filter_query='',
                 sort_action='native',
                 sort_mode='single',
                 sort_by=[],
                 style_as_list_view=True,
-                hidden_columns=[],
             ),
         ]),
         dbc.Row([
             dbc.Col([
                 html.H5("Age/Gender Distribution"),
-
                 dcc.Graph(id='pie-chart'),
-
                 html.P("Names:"),
                 dcc.Dropdown(id='names',
                              options=['Age Group', 'Gender'],
                              value='Age Group',
                              clearable=False
                              ),
-
                 html.P("Values:"),
                 dcc.Dropdown(id='values',
                              options=['Age Place'],
@@ -123,19 +102,14 @@ app.layout = html.Div([
             ]),
             dbc.Col([
                 html.H5('Age vs Finish for both Genders'),
-
                 dcc.Graph(
                     id='graph-with-slider',
-                    # figure=scat
                 ),
-
                 dcc.Slider(
                     id='scat-place-slider',
                     min=reduced2['Gender Place'].min(),
                     max=200,
                     value=reduced2['Gender Place'].min(),
-                    # marks={str(year): str(year) for year in reduced2['Gender Place'].unique()},
-                    step=None,
                     marks={
                         10: '10',
                         25: '25',
@@ -145,19 +119,14 @@ app.layout = html.Div([
                 ),
             ]),
         ]),
-
         dcc.Graph(
             id='par-with-slider',
-            # figure=para_cor
         ),
-
         dcc.Slider(
             id='par-place-slider',
             min=reduced2['Gender Place'].min(),
             max=200,
             value=reduced2['Gender Place'].min(),
-            # marks={str(year): str(year) for year in reduced2['Gender Place'].unique()},
-            step=None,
             marks={
                 10: '10',
                 25: '25',
@@ -166,7 +135,6 @@ app.layout = html.Div([
                 200: '200'
             }
         ),
-
     ])
 ])
 
@@ -185,22 +153,19 @@ def update_figure_pie(names, values):
     Input(component_id='scat-place-slider', component_property='value'))
 def update_figure_scat(places):
     filtered_df = females[females['Gender Place'] <= places]
-
-    # create the plots
     scat = px.scatter(filtered_df, x=filtered_df['Age'], y=filtered_df['Gender Place'])
     scat.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
         font_color=colors['text']
     )
-
     return scat
 
 
 @app.callback(
     Output(component_id='par-with-slider', component_property='figure'),
     Input(component_id='par-place-slider', component_property='value'))
-def update_figure_scat(places):
+def update_figure_parcoords(places):
     reduced3 = females[["Name", "Swim Minutes", "Swim+T1",
                         "Plus Bike", "Plus T2", "Total", "Gender Place"]]
     reduced3["Start"] = 0
@@ -208,12 +173,9 @@ def update_figure_scat(places):
     reduced3 = reduced3[reduced3['Total'] > 60]
     reduced3 = reduced3[reduced3['Gender Place'] >= 1]
     reduced3 = reduced3[reduced3['Gender Place'] <= places]
-    # print(reduced3)
 
-    # create the para coord plot
-    dimensions = list([
-        dict(range=[0, 1],
-             label='Start', values=reduced3['Start']),
+    dimensions = [
+        dict(range=[0, 1], label='Start', values=reduced3['Start']),
         dict(range=[reduced3["Swim Minutes"].min(), reduced3["Swim Minutes"].max()],
              label='Time After Swim', values=reduced3['Swim Minutes']),
         dict(range=[reduced3["Swim+T1"].min(), reduced3["Swim+T1"].max()],
@@ -224,9 +186,10 @@ def update_figure_scat(places):
              label='Time After Second Transition', values=reduced3['Plus T2']),
         dict(range=[reduced3["Total"].min(), reduced3["Total"].max()],
              label='Total Time', values=reduced3['Total']),
-        dict(range=[0, reduced3['Gender Place'].max()], tickvals=reduced3['Gender Place'], ticktext=reduced3['Name'],
+        dict(range=[0, reduced3['Gender Place'].max()],
+             tickvals=reduced3['Gender Place'], ticktext=reduced3['Name'],
              label='Competitor', values=reduced3['Gender Place'])
-    ])
+    ]
 
     para_cor = go.Figure(data=go.Parcoords(line=dict(color=reduced3['Gender Place'],
                                                      colorscale=[[.0, 'rgba(255,0,0,0.1)'], [0.2, 'rgba(0,255,0,0.1)'],
@@ -244,7 +207,6 @@ def update_figure_scat(places):
         font_color=colors['text'])
 
     return para_cor
-
 
 # if __name__ == '__main__':
 #     app.run_server(debug=True)
